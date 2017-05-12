9b973f905d73ab7d194d55077b0c35b2ebfc3693
Wyatt <Pratt@Wyatts-MacBook-Pro.local>
2017-05-11 18:52:28 -0700

    adding even odd code

---
+++ src/com/company/EvenOddCheck.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class EvenOddCheck
+{
+
+    public static void main(String args [])
+    {
+            int num=1321;
+
+            if(num%2==0)
+                    System.out.println("hello world");
+            else
+
+                    System.out.println("not this time sucker");
+
+
+    }
+}
