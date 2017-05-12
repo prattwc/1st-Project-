af89003c181592cdd6eb471a4b014ff58b77af94
prattwc <wypratt@icloud.com>
2017-05-12 15:43:35 -0700

    Add files via upload

---
+++ SoldiersField.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Pratt on 5/12/17.
+ */
+public class SoldiersField
+{
+    public static void main(String args [])
+    {
+        int num = -1322;
+
+        if (num%2==0)
+            System.out.println("hello world");
+        else
+
+            System.out.println("not this time sucker");
+
+    }
+
+ }
