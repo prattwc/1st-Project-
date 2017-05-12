026405fc8f5d496caf14791c3b26da8c572726af
Sophie Panuthos <spanuthos@apple.com>
2017-05-11 19:14:32 -0700

    adding code to helloWorld

--- assigned/src/HelloWorld.java
+++ assigned/src/HelloWorld.java
@@ -2,4 +2,17 @@
  * Created by spanut on 5/11/17.
  */
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        printHelloWorld(0);
+        printHelloWorld(1);
+    }
+
+    // printHelloWorld with an even number should print "hello world"
+    // printHelloWorld with a odd number should print "not this time sucker"
+
+    public static void printHelloWorld(int input) {
+        System.out.println("Hello World");
+    }
+
 }
